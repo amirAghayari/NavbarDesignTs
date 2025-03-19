bb5c62f20408015727008eb15ccb0481c85a2b6b
amirAghayari <amiraghayari2119@gmail.com>
2025-03-19 08:36:13 +0330

    restart design

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,20 +1,92 @@
-import { RxHamburgerMenu } from "react-icons/rx";
-import { TbBrandGmail } from "react-icons/tb";
-import { InputDemo } from "./ui/InputDemo";
-import { FaRegQuestionCircle } from "react-icons/fa";
-import { CiSettings } from "react-icons/ci";
-import { CgProfile } from "react-icons/cg";
+import { useState } from "react";
+import { Catalog } from "./Catalog";
+import { CiSearch } from "react-icons/ci";
+import { IoMdMenu } from "react-icons/io";
 
 const Navbar: React.FC = () => {
+  const [darkMode, setDarkMode] = useState<boolean>(false);
+
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+    document.documentElement.classList.toggle("dark", !darkMode);
+  };
   return (
-    <div className="w-screen h-15 bg-gray-100 flex items-center">
-      <RxHamburgerMenu className="w-10 h-10 ml-10" />
-      <TbBrandGmail className="w-20 h-14 ml-4" />
-      <h1 className="text-3xl font-bold">Gmail</h1>
-      <InputDemo />
-      <FaRegQuestionCircle className="w-10 h-10 ml-15 " />
-      <CiSettings className="w-10 h-10 ml-8" />
-      <CgProfile className="w-10 h-10 ml-8" />
+    <div
+      className={` ${
+        darkMode ? `!bg-[#151d20]` : `!bg-[#fbfbfb]`
+      } w-screen h-20 flex items-center justify-between`}
+    >
+      {/* logo */}
+      <div className="font-bold text-2xl ml-10 p-2 border-8 border-solid border-amber-500 border-b-0">
+        GOAL
+      </div>
+
+      {/* Middle */}
+      <div className="flex items-center justify-center gap-4">
+        <a
+          href="#"
+          className={`${
+            darkMode ? "!text-gray-100" : "!text-gray-800"
+          } text-xl font-bold`}
+        >
+          About
+        </a>
+        <select
+          defaultValue="Catalog"
+          className="select select-ghost font-bold select-md text-xl text-gray-500"
+        >
+          <option disabled={true}>Catalog </option>
+          <option>Catalog</option>
+        </select>
+
+        <select
+          defaultValue="places"
+          className="select select-ghost font-bold select-md text-xl text-gray-500"
+        >
+          <option disabled={true}>Places </option>
+          <option>Places</option>
+        </select>
+
+        <a href="#" className="!text-gray-500 text-xl font-bold">
+          Blog
+        </a>
+
+        <a href="#" className="!text-gray-500 text-xl font-bold">
+          Content
+        </a>
+      </div>
+
+      {/* left */}
+
+      <div className="flex items-center justify-center gap-4 mr-10">
+        <CiSearch className="w-6 h-6 text-gray-500" />
+        <a href="#" className="!text-gray-500 text-xl font-bold">
+          Login
+        </a>
+
+        <div
+          className={` ${
+            darkMode
+              ? `bg-[#fbfbfb] text-[#151d20]`
+              : `bg-[#151d20] text-[#fbfbfb]`
+          } flex flex-row items-center justify-between rounded-4xl gp-2 p-2 w-30`}
+        >
+          <IoMdMenu className="w-8 h-8" />
+          <h1 className="!text-lg font-bold">Menu</h1>
+        </div>
+      </div>
+
+      {/* switchTheme btn */}
+      <button
+        onClick={toggleDarkMode}
+        className={`fixed bottom-4 right-4 p-4 rounded-full shadow-lg ${
+          darkMode
+            ? "bg-gray-800 hover:bg-gray-700"
+            : "bg-blue-500 hover:bg-blue-600"
+        } text-white transition-colors z-50`}
+      >
+        {darkMode ? "☀️" : "🌙"}
+      </button>
     </div>
   );
 };
